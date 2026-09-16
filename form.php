11fbf73239c9199fc742566a5d9fd25523a57544
Micah-Duff <md0992077@otc.edu>
2026-09-16 09:21:04 -0500

    Created basic form that returns values and plugged in bootstrap

---
+++ form.php
@@ -0,0 +1,11 @@
+<form style="margin: 10px; padding: 10px;">
+    <div>
+        <label for="returnDate">Return Date:</label>
+        <input type="date" name="returnDate">
+        <br>
+        <label for="dueDate">Due Date:</label>
+        <input type="date" name="dueDate">
+    </div>
+    <hr>
+    <input type="submit" value="Check-in Book">
+</form>
